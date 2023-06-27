7b440ff19b1494771c4de128d6a902792badce9a
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-27 10:50:03 +0530

    added email

--- UserRegTest/UnitTest1.cs
+++ UserRegTest/UnitTest1.cs
@@ -22,5 +22,14 @@ namespace UserRegTest
             bool result = methood.FirstName(name);
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        [DataRow("sukanya@gmail.con", true)]
+        [DataRow("sukanya@com", false)]
+        public void CheckEmail_SHouldReturn_True_IfValid(string email, bool expected)
+        {
+            bool result = methood.Email(email);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
--- Userregnormal/UserReg.cs
+++ Userregnormal/UserReg.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Userregnormal
 {
@@ -13,6 +14,20 @@ namespace Userregnormal
         {
             Regex Checkname = new Regex("^[A-Z][a-z]{2,}");
             Match match = Checkname.Match(name);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Email(string email)
+        {
+            Regex Checkemail = new Regex("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
+            Match match = Checkemail.Match(email);
             if (match.Success)
             {
                 return true;
